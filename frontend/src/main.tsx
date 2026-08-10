0b930d9ef8eb002a87897389b2299c3357e34264
markwelshboy <mark.david.richards@gmail.com>
2026-08-10 08:37:58 -0700

    Load refined image prep stage layout

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,6 +7,7 @@ import "./styles.css";
 import "./data-ui.css";
 import "./prep-refinements.css";
 import "./derivative-hover.css";
+import "./image-prep-stage1.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
