7e2476ae81298723d720f4ffbd197957164c68ce
markwelshboy <mark.david.richards@gmail.com>
2026-08-12 10:27:52 -0700

    Load training filename styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -14,6 +14,7 @@ import "./caption-workbench.css";
 import "./caption-toast.css";
 import "./start-source-layout.css";
 import "./image-helper-overlays.css";
+import "./training-filenames.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
