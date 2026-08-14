8d48ca8c30d771d4bc3e3355b34c93a8cec361da
markwelshboy <mark.david.richards@gmail.com>
2026-08-13 18:57:25 -0700

    Load caption methodology styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -13,6 +13,7 @@ import "./exception-editor-polish.css";
 import "./caption-workbench.css";
 import "./caption-review-pager.css";
 import "./caption-template.css";
+import "./caption-methodologies.css";
 import "./caption-toast.css";
 import "./start-source-layout.css";
 import "./image-helper-overlays.css";
