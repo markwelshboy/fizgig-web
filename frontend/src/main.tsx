53651c5135b16de36f252da7b9a41b2164bfe5de
markwelshboy <mark.david.richards@gmail.com>
2026-08-14 21:09:25 -0700

    ui: load Tabler icon webfont

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { SessionProvider } from "./session";
+import "@tabler/icons-webfont/dist/tabler-icons.min.css";
 import "./styles.css";
 import "./data-ui.css";
 import "./prep-refinements.css";
